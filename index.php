2017c1b872d21a49827c963373ca6520c4212508
Alloy22 <aloysius22@hotmail.sg>
2022-10-13 15:32:24 +0800

    config files

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-            echo "Testing 123 321 123321123123132313"
+            echo "Testing 123"
         ?>
     </body>
 </html>
