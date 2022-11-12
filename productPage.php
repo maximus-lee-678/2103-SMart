38fb6305128637e5df077765a471b895e1905cdf
Alloy22 <aloysius22@hotmail.sg>
2022-11-12 22:07:44 +0800

    Show Similar Products (Product Page)

--- productPage.php
+++ productPage.php
@@ -2,7 +2,7 @@
     <title>Shop</title>
     <?php
     session_start();
-    
+
     include "helper-functions.php";
     include "head.php";
 
@@ -22,7 +22,7 @@
         $product_id = sanitize_input($_GET["id"]);
 
         // Prepare the statement:
-        $stmt = $conn->prepare("SELECT p.name AS product_name, p.display_unit, p.price, p.image_url, p.quantity, c.name AS category_name, s.name AS supermarket_name, b.name AS brand_name FROM Product AS p INNER JOIN Supermarket AS s ON p.sm_id = s.id INNER JOIN Category AS c ON p.cat_id = c.id INNER JOIN Brand AS b ON p.brand_id = b.id WHERE p.id = ? AND p.active = 1");
+        $stmt = $conn->prepare("SELECT p.name AS product_name, p.display_unit, p.price, p.image_url, p.quantity, p.cat_id, c.name AS category_name, s.name AS supermarket_name, b.name AS brand_name FROM Product AS p INNER JOIN Supermarket AS s ON p.sm_id = s.id INNER JOIN Category AS c ON p.cat_id = c.id INNER JOIN Brand AS b ON p.brand_id = b.id WHERE p.id = ? AND p.active = 1");
         // Bind & execute the query statement:
         $stmt->bind_param("i", $product_id);
         // execute the query statement:
@@ -38,11 +38,24 @@
                 $image_url = $row["image_url"];
                 $category_name = $row["category_name"];
                 $quantity = $row["quantity"];
+                $category_id = $row["cat_id"];
                 $supermarket_name = $row["supermarket_name"];
                 $brand_name = $row["brand_name"];
+
+                $stmt->close();
+
+                $stmt = $conn->prepare("SELECT * FROM Product_Test WHERE MATCH(name) AGAINST (? IN NATURAL LANGUAGE MODE) AND cat_id = ? ORDER BY RAND() LIMIT 5;");
+
+                $stmt->bind_param("ss", $product_name, $category_id);
+
+                if (!$stmt->execute()) {
+                    $captionText = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+                } else {
+                    $result = $stmt->get_result();
+                }
             }
+            $stmt->close();
         }
-        $stmt->close();
     }
     $conn->close();
     ?>    
@@ -77,8 +90,44 @@
                     <p><br>rating? review?</p> <!--idk lol-->
                     <!--if got purchase history-->
                 </div>
+
+
             </div>
 
+        </section>
+        <section class ="products">
+            <div class="box-container" id = "product-list">
+                <?php
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        ?>
+                        <div class="box" id="<?= $row["id"] ?>">
+                            <div class="icons">
+                                <a href="#" class="fas fa-shopping-cart add-to-cart"></a>
+                                <a href="#" class="fas fa-heart"></a>
+                                <a href="productPage.php?id=<?= $row["id"] ?>" class="fas fa-eye"></a>
+                            </div>
+                            <div class="image">
+                                <img src="<?= $row["image_url"] ?>" alt="">
+                            </div>
+                            <div class="content">
+                                <h3><?= $row["name"] ?></h3>
+                                <div class="price">$<?= $row["price"] ?></div>
+                                <div class="stars">
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="fas fa-star"></i>
+                                    <i class="far fa-star"></i>
+                                </div>
+                            </div>
+                        </div>
+                        <?php
+                    }
+                }
+                ?>
+
+            </div>
         </section>
         <?php include "footer.php"; ?>
     </body>
